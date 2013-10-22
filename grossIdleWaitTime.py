84425ff82c4bf9d0fb1a20d0924d3c276a0afdf8
carche <carmenc.che@gmail.com>
2013-10-22 18:24:12 -0300

    Update grossIdleWaitTime.py
    Single report: can input building OR numerical goal.

--- grossIdleWaitTime.py
+++ grossIdleWaitTime.py
@@ -2,13 +2,20 @@
 # Repository : cookie-clicker
 # INFO : Calculates gross idle wait time to next achievement.
 
-# TODO: single report, flexible input
-# TODO: format large/specific numbers
+# TODO: improve runtime speed
+# TODO: format large/specific numbers; rounding
 # TODO: read directly from game stats?
+
+import sys
 
 """
 FUNCTION DEFINITIONS
 """
+
+# Called upon KeyboardInterrupt or "Exit" input
+def handleExit():
+   print "Exiting."
+   sys.exit(1)
 
 # Comprehensive time conversion
 def timeReport(seconds):
@@ -26,36 +33,30 @@ def timeReport(seconds):
     return ""
 
 # Summarize status and wait time
-def cookieReport(INFLATION, CPS, CURRENT_STOCK, TARGET_QUOTA, targetCookies, BUILDING):
+def cookieReport(INFLATION, CPS, currentStock, targetQuota, targetCookies, building):
 
-    # Current Status
-    if TARGET_QUOTA != 1:
-        print "Item:\t%s" % BUILDING
-        print "Have:\t%d" % CURRENT_STOCK
-        print "Need:\t%d (%d more)" % (TARGET_QUOTA, (TARGET_QUOTA - CURRENT_STOCK))
-    
     # Ouput cookies to goal (assume inBank = 0)
     i = 0 # buildings to quota
     cookiesToQuota = 0
-    while i < (TARGET_QUOTA - CURRENT_STOCK):
+    while i < (targetQuota - currentStock):
         cookiesToQuota = cookiesToQuota + targetCookies * (INFLATION ** i)
         i = i + 1
-    if TARGET_QUOTA - CURRENT_STOCK > 1:
+    if targetQuota - currentStock > 1:
         print "Next item cost: \t%d" % targetCookies
         print "Cumulative cost:\t%d (%d)" % (long(cookiesToQuota), int(cookiesToQuota))
     
     # Output corresponding idle wait time
     grossTime = targetCookies / CPS
     print "Minutes for next purchase:\t", timeReport(grossTime)
-    if TARGET_QUOTA - CURRENT_STOCK > 1:
+    if targetQuota - currentStock > 1:
         grossTime = cookiesToQuota / CPS
         print "Minutes for target quota:\t", timeReport(grossTime)
+    return ""
 
 """
 INITIALIZATION
 """
-
-# Constants
+ 
 BASE_COST = {
     'CURSOR':15,
     'GRANDMA':100,
@@ -65,33 +66,44 @@ BASE_COST = {
     'SHIPMENT':40000,
     'ALCHEMY':200000,
     'PORTAL':1666666,
-    'TIME_MACHINE':123456789,
+    'TIMEMACHINE':123456789,
     'ANTIMATTER':3999999999}
 INFLATION = 1.15 # building price increase ratio
 
-# Input
 CPS = 5705693990.7 # cookies per second
-BUILDING = 'grandma'.upper() # BASE_COST key
-CURRENT_STOCK = 150 # Minimum: 0
-TARGET_QUOTA = 200 # Minimum: 1
+
+building = "" # BASE_COST key
 
 """
 MAIN
 """
 
 print "INFO: Calculates gross idle wait time to next achievement."
-print "Cookies per second:\t", CPS, "\n"
+print "Cookies per second:\t%d\n" % CPS
 
-# Long-term calculation - building accumulator
-targetCookies = long(BASE_COST[BUILDING] * INFLATION ** CURRENT_STOCK) # cost of next building upgrade
-cookieReport(INFLATION, CPS, CURRENT_STOCK, TARGET_QUOTA, targetCookies, BUILDING)
-print ""
+while building.upper() not in BASE_COST:
+   try: # Case: cookie (numerical) input
+      target = raw_input("Enter target:\t")
+      targetCookies = long(target)
+      building = None
+      currentStock = 0
+      targetQuota = 1
+      break
+   except ValueError: # Case: building (string) input
+      if target.lower() == "exit":
+         handleExit()
+      else:
+         try: 
+            building = target.upper()
+            targetCookies = BASE_COST[building]
+         except KeyError:
+            print "Invalid building name. Try one of the following:\n", BASE_COST.keys()
+   except KeyboardInterrupt:
+      handleExit()
+if isinstance(building, str): # Building entered, not cookies
+   currentStock = int(raw_input("Have:\t",))
+   targetQuota = int(raw_input("Need:\t",))
+   print "(%d more)\n" % (targetQuota - currentStock)
+   targetCookies = long(BASE_COST[building] * INFLATION ** currentStock) # cost of next building upgrade
 
-# Optional calculation - single target
-try:
-    targetCookies = int(raw_input("Enter target cookies: \t"))
-    cookieReport(1, CPS, 0, 1, targetCookies, None)
-except ValueError:
-    print "Invalid input; exiting program."
-except KeyboardInterrupt:
-    print ""
+cookieReport(INFLATION, CPS, currentStock, targetQuota, targetCookies, building) 
