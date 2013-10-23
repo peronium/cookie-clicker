886f833497a0745ab72830fb9819d681cc706430
carche <carmenc.che@gmail.com>
2013-10-23 10:45:43 -0300

    Update grossIdleWaitTime.py
    - Added Have/Need validator
    - Made PRECISION variable dynamic
    - Minor output reformatting

--- grossIdleWaitTime.py
+++ grossIdleWaitTime.py
@@ -2,8 +2,10 @@
 # Repository : cookie-clicker
 # INFO : Calculates gross idle wait time to next achievement.
 
+# TODO: change CPS in prompt
+# TODO: order keys in ouput
+# TODO: fix rounding: precision; format large/specific numbers
 # TODO: improve runtime speed
-# TODO: format large/specific numbers; rounding
 # TODO: read directly from game stats?
 
 import sys
@@ -18,10 +20,10 @@ def handleExit():
    sys.exit(1)
 
 # Comprehensive time conversion
-def timeReport(seconds):
-    minutes = round(seconds / 60.0, 3)
-    hours = round(seconds / 3600.0, 3)
-    days = round(seconds / 86400.0, 3)
+def timeReport(seconds, PRECISION):
+    minutes = round(seconds / 60.0, PRECISION)
+    hours = round(seconds / 3600.0, PRECISION)
+    days = round(seconds / 86400.0, PRECISION)
     if minutes < 1:
         print "%d secs" % seconds,
     elif minutes < 5:
@@ -30,28 +32,27 @@ def timeReport(seconds):
         print "%d mins\t(%d hours)" % (minutes, hours),
     else:
         print "%d hours\t(%d DAYS)" % (hours, days),
-    return ""
+    return "\n"
 
 # Summarize status and wait time
-def cookieReport(INFLATION, CPS, currentStock, targetQuota, targetCookies, building):
+def cookieReport(INFLATION, PRECISION, CPS, currentStock, targetStock, targetCookies):
 
     # Ouput cookies to goal (assume inBank = 0)
     i = 0 # buildings to quota
     cookiesToQuota = 0
-    while i < (targetQuota - currentStock):
+    while i < (targetStock - currentStock):
         cookiesToQuota = cookiesToQuota + targetCookies * (INFLATION ** i)
         i = i + 1
-    if targetQuota - currentStock > 1:
+    if targetStock - currentStock > 1:
         print "Next item cost: \t%d" % targetCookies
         print "Cumulative cost:\t%d (%d)" % (long(cookiesToQuota), int(cookiesToQuota))
     
     # Output corresponding idle wait time
     grossTime = targetCookies / CPS
-    print "Minutes for next purchase:\t", timeReport(grossTime)
-    if targetQuota - currentStock > 1:
+    print "Minutes for next purchase:\t", timeReport(grossTime, PRECISION),
+    if targetStock - currentStock > 1:
         grossTime = cookiesToQuota / CPS
-        print "Minutes for target quota:\t", timeReport(grossTime)
-    return ""
+        print "Minutes for target quota:\t", timeReport(grossTime, PRECISION)
 
 """
 INITIALIZATION
@@ -69,6 +70,7 @@ BASE_COST = {
     'TIMEMACHINE':123456789,
     'ANTIMATTER':3999999999}
 INFLATION = 1.15 # building price increase ratio
+PRECISION = 2 # rounding precision
 
 CPS = 5705693990.7 # cookies per second
 
@@ -78,8 +80,8 @@ building = "" # BASE_COST key
 MAIN
 """
 
-print "INFO: Calculates gross idle wait time to next achievement."
-print "Cookies per second:\t%d\n" % CPS
+print "INFO: Calculates gross idle wait time to next achievement.\n"
+print "Cookies per second:\t%d" % CPS
 
 while building.upper() not in BASE_COST:
    try: # Case: cookie (numerical) input
@@ -87,8 +89,9 @@ while building.upper() not in BASE_COST:
       targetCookies = long(target)
       building = None
       currentStock = 0
-      targetQuota = 1
+      targetStock = 1
       break
+      
    except ValueError: # Case: building (string) input
       if target.lower() == "exit":
          handleExit()
@@ -101,9 +104,19 @@ while building.upper() not in BASE_COST:
    except KeyboardInterrupt:
       handleExit()
 if isinstance(building, str): # Building entered, not cookies
-   currentStock = int(raw_input("Have:\t",))
-   targetQuota = int(raw_input("Need:\t",))
-   print "(%d more)\n" % (targetQuota - currentStock)
+   while True:
+      try:
+         currentStock = int(raw_input("Have: "))
+         targetStock = int(raw_input("Need: "))
+         if targetStock > currentStock: # inputs are valid
+            break
+         else:
+            print "\"Need\" value must be greater than \"Have\" value. Please try again."
+      except ValueError:
+         print "Invalid input. Please try again."
+      except KeyboardInterrupt:
+         handleExit()
+   print "(%d more)\n" % (targetStock - currentStock)
    targetCookies = long(BASE_COST[building] * INFLATION ** currentStock) # cost of next building upgrade
 
-cookieReport(INFLATION, CPS, currentStock, targetQuota, targetCookies, building) 
+cookieReport(INFLATION, PRECISION, CPS, currentStock, targetStock, targetCookies) 
